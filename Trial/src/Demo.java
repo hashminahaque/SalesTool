ff907207579149c6fff3f7f353705c1461985892
hashmina haque <hashmina007@gmail.com>
2015-06-02 16:31:47 +0530

    Initial checking of sales tool.

---
+++ Trial/src/Demo.java
@@ -0,0 +1,19 @@
+
+public class Demo {
+	public static void main(String args[])
+	{
+	 SalesData data=new SalesData();
+	 displayGreeting();
+	 data.display();
+	
+	}
+
+
+
+private static void displayGreeting()
+{
+ System.out.println("Hello Happy Sales People!");
+ System.out.println("This app shows sales data");
+
+}
+}
