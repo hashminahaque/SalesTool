4f4922470c419d1f1c093c871e25126ccb087f91
hashmina haque <hashmina007@gmail.com>
2015-06-02 16:44:59 +0530

    Add a simple output change

--- Trial/src/Demo.java
+++ Trial/src/Demo.java
@@ -14,6 +14,6 @@ private static void displayGreeting()
 {
  System.out.println("Hello Happy Sales People!");
  System.out.println("This app shows sales data");
-
+ System.out.println("Test 1");
 }
 }
